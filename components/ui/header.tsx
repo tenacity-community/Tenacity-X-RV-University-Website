662d52fce30e47a8d50af32d6d81eed853d768e7
Taufeeq <taufeeq.riyaz64@gmail.com>
2025-02-04 16:50:39 +0530

    minor optimizations

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -11,24 +11,28 @@ export default function Header() {
         <div className="relative flex h-14 items-center justify-center gap-3 rounded-2xl bg-white/90 px-3 shadow-lg shadow-black/[0.03] backdrop-blur-sm before:pointer-events-none before:absolute before:inset-0 before:rounded-[inherit] before:border before:border-transparent before:[background:linear-gradient(theme(colors.gray.100),theme(colors.gray.200))_border-box] before:[mask-composite:exclude_!important] before:[mask:linear-gradient(white_0_0)_padding-box,_linear-gradient(white_0_0)]">
           {/* Site branding */}
           <div className="flex flex-1 space-x-8 justify-center md:justify-start items-center">
+            <Link href="/">
             <Image
               src={TenacityLogo}
               width={90}
               height={90}
-              alt="Picture of the author"
+              alt="Tenacity Logo"
             />
+            </Link>
+            <Link href={"/"}>
             <Image
               src={RVULogo}
               width={70}
               height={70}
-              alt="Picture of the author"
-            />
+              alt="RV University Logo"
+              /></Link>
+          <Link href={"/"}>
             <Image
               src={DevSphereLogo}
               width={100}
               height={100}
-              alt="Picture of the author"
-            />
+              alt="DevSphere Logo"
+          /></Link>
           </div>
 
           <ul className="flex flex-1 items-center md:block justify-end gap-3">
